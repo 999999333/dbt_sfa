891c011bca20cbd9af6f3883aa607bfdc173167b
Karlik <karel.vitek@mattoni.cz>
2024-01-05 13:52:16 +0100

    init sales real

--- models/marts/sales_datamart/sales_real.sql
+++ models/marts/sales_datamart/sales_real.sql
@@ -1,6 +1,24 @@
 {{
     config(
-        materialized='table'
+        materialized='view'
     )
 }}
 
+with kmv_open as (
+    select * from {{ ref('int_kmv_documents_open_enhanced') }}
+),
+
+kmv_closed as (
+    select * from {{ ref('int_kmv_documents_closed_enhanced') }}
+),
+
+final as (
+
+    select * from kmv_open
+
+    union all
+
+    select * from kmv_closed
+)
+
+select * from final
