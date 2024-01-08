a52da0bbd70c45420e96e159aed5329a63ec6fdb
Karlik <karel.vitek@mattoni.cz>
2024-01-08 07:50:50 +0100

    change sales real to dbt join macro

--- models/marts/sales/sales_real.sql
+++ models/marts/sales/sales_real.sql
@@ -12,13 +12,27 @@ kmv_closed as (
     select * from {{ ref('int_kmv_documents_closed_enhanced') }}
 ),
 
+pepsi_open as (
+    select * from {{ ref('int_pepsi_documents_open_enhanced') }}
+),
+
+pepsi_closed as (
+    select * from {{ ref('int_pepsi_documents_closed_enhanced') }}
+),
+
 final as (
+    {{ dbt_utils.union_relations(
+        relations=[
+            ref('int_kmv_documents_closed_enhanced'),
+            ref('int_kmv_documents_open_enhanced'),
+            ref('int_pepsi_documents_closed_enhanced'),
+            ref('int_pepsi_documents_open_enhanced'),
+            ]
+        )
+    }}
+    
 
-    select * from kmv_open
 
-    union all
-
-    select * from kmv_closed
 )
 
 select * from final
